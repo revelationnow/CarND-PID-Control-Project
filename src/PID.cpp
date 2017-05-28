180550cb4594b92ddc0729bb6ef8c69bb06aaab8
Anoop Ramakrishna <revelationnw@hotmail.com>
2017-05-28 13:50:54 -0700

    First commit: Added PID functions

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
+
 }
 
 double PID::TotalError() {
+  return (Kp * p_error) + (Kd * d_error) + (Ki * i_error);
 }
 
